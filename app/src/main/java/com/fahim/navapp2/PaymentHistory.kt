a3926297686aa6382ad9785e757ec079a486c350
versionV2 <f.sm151496@gmail.com>
2021-09-02 11:13:59 +0600

    new update

--- app/src/main/java/com/fahim/navapp2/PaymentHistory.kt
+++ app/src/main/java/com/fahim/navapp2/PaymentHistory.kt
@@ -14,16 +14,6 @@ import com.google.gson.Gson
 import data.ResponseClient
 import viewModels.PaymentHistoryViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PaymentHistory.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PaymentHistory : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -33,13 +23,6 @@ class PaymentHistory : Fragment() {
     lateinit var binding: FragmentPaymentHistoryBinding
     lateinit var allinfo:ResponseClient
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -64,23 +47,4 @@ class PaymentHistory : Fragment() {
 
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PaymentHistory.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PaymentHistory().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
